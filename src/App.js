ba3e4c688878e9f142c20c9b216757107fa29b9c
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-02 14:18:05 +0700

    fix navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import { routes } from './routes'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import HeaderComponent from './components/HeaderComponent/HeaderComponent'
+import HomePage from './pages/HomePage/HomePage'
+import ProductsPage from './pages/ProductsPage/ProductsPage'
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          {routes.map((route) => {
-            const Layout = route.isShowHeader ? DefaultComponent : Fragment
-            return (
-
-              <Route key={route.path} path={route.path} element={
-                <Layout>
-                  <route.page />
-                </Layout>
-              } />
-
-            )
-          })}
-        </Routes>
-      </Router>
+    <div className='App'>
+      <HeaderComponent/>
+      <Routes>
+        <Route index element={<HomePage/>}/>
+      </Routes>
+      <Routes>
+        <Route path='/Home'element={<HomePage/>}/>
+        <Route path='/Shop'element={<ProductsPage/>}/>
+        <Route path='/CreateYourOwn'element={<ProductsPage/>}/>
+        <Route path='/About'element={<ProductsPage/>}/>
+        <Route path='/Contact'element={<ProductsPage/>}/>
+        <Route path='/SizeGuide'element={<ProductsPage/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/NavBarHeaderComponent/NavBarHeaderComponent.jsx
+++ src/components/NavBarHeaderComponent/NavBarHeaderComponent.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { NavBarHeaderItem } from '../../Shared/ListNavBerHeader';
-
-const NavBarHeaderComponent = () => {
-    return (
-        <nav className='nav-bar-header'>
-            <ul className='nav-bar-header-list'>
-                {NavBarHeaderItem.map((item, index) => (
-                    <li className='nav-bar-header-item' key={index}>
-                        <a href='#'>{item}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
-};
-
-export default NavBarHeaderComponent;
+import React from 'react'
+import './NavBarHeader.css'
+import { Link } from 'react-router-dom'
+export default function 
+() {
+  return (
+    <div className='nav-bar-header'>
+    <nav>
+        <ul className='menu'>
+        <div className='navmenu'>
+                  <li><Link to='/Home'>Home</Link></li>
+                  <li><Link to='/Shop'>Shop</Link></li>
+                  <li><Link to='/CreateYourOwn'>Create Your Own</Link></li>
+                  <li><Link to='/About'>About Us</Link></li>
+                  <li><Link to='/Contact'>Contact</Link></li>
+                  <li><Link to='/SizeGuide'>Size Guide</Link></li>
+                  </div>
+        </ul>
+    </nav>
+    </div>
+  )
+}
